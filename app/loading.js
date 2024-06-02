4d1930aaefb28a345ece4496dd57cf2257c0d00d
Arman-Kabir <armankabir8@gmail.com>
2024-06-02 13:49:00 +0600

    loading

---
+++ app/loading.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Loading() {
+    console.log('hello b')
+  return (
+    <div>Loading About segment</div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
+// "use client";
 import Image from "next/image";
 
 export default function Home() {
+  console.log('hello world');
   return (
     <main className="mt-10">
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad error aspernatur voluptas, praesentium eligendi vel possimus? Commodi est aut in!
