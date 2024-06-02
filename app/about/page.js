07835f60f42aa08f4e08ea3c7fd8c9573d3127eb
Arman-Kabir <armankabir8@gmail.com>
2024-06-02 14:21:24 +0600

    error not-found

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,7 @@
+// import Error from "next/error"
 
 export default function About() {
+    // throw new Error('There was a hug error');
     return (
         <main className="mt-10">
             <div>about us page</div>
