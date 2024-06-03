e6069d4525fa184d450325a5dc78b5784737c663
Arman-Kabir <armankabir8@gmail.com>
2024-06-03 21:00:14 +0600

    post comments

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,13 +1,39 @@
 import getPost from "@/lib/getPost";
+import getPostComments from "@/lib/getPostComment";
+
+export async function generateMetadata({ params }) {
+    const { id } = params;
+    const post = await getPost(id);
+    // const comments = await getPostComments(id);
+    return {
+        title: post.title,
+        description: post.body
+    }
+}
 
 export default async function PostPage({ params }) {
     const { id } = params;
-    const post = await getPost(id);
+    const postPromise = getPost(id);
+    const commentsPromise = getPostComments(id);
+
+    const [post, comments] = await Promise.all([postPromise, commentsPromise]);
+    console.log(comments);
+
+
     // console.log(post);
     return (
         <div className="mt-6">
-            <h2 className="text-blue">{post.title}</h2>
+            <h2 className="text-blue-600">{post.title}</h2>
             <p className="mt-3">{post.body}</p>
+            <hr></hr>
+            <div className="mt-10">
+                <h1>comments</h1>
+                <ul>
+                    {
+                        comments.map(comment => <li className="mb-3" key={comment.id}>{comment.body}</li>)
+                    }
+                </ul>
+            </div>
         </div>
     )
 }
